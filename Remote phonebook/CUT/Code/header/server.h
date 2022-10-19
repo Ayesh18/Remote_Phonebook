6995d742f385dd3946f14e9964218b96120700d7
sainathreddy <70091863+sainath099@users.noreply.github.com>
2022-10-20 00:39:54 +0530

    Update server.h

--- Remote phonebook/CUT/Code/header/server.h
+++ Remote phonebook/CUT/Code/header/server.h
@@ -3,8 +3,8 @@
 **
 **    DESCRIPTION    :      This File contains server header files.
 *****************************************************************************************/
-
-
+#ifndef SERVER_H
+#define SERVER_H
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/ipc.h>
@@ -19,22 +19,22 @@
 #define MAX 256
 #define PORT 8028
 											
-void load();	    										//load the data from the file
+void load();	    								//load the data from the file
 
-int ToCreateSocket();											//create the socket
-int ToBindSocket();											//to bind the socket	
-int ToListen(); 											//to listen to the socket 
+int ToCreateSocket();								//create the socket
+int ToBindSocket();								//to bind the socket	
+int ToListen(); 							        //to listen to the socket 
 		
-void ToAcceptConnections();										//to accept connections
-void SToRecvData(char *);										//to receive data and commands from client
-void ToAuthenticateUser();										//to authenticate the user
+void ToAcceptConnections();							//to accept connections
+void SToRecvData(char *);							//to receive data and commands from client
+void ToAuthenticateUser();							//to authenticate the user
 		
 
 void AuthenticatedUserFunctionalities(char *,char * ,char * );			//to perform authenticated user functionalities
-void AnonymousFunctions(char *recvdata,char * filename);			//to perform anonymous user functionalities
-void AdminFunction(char *recvdata,char * filename);				//to perform admin user functionalities
-		
+void AnonymousFunctions(char *,char * );			//to perform anonymous user functionalities
+void AdminFunction(char *,char * );				//to perform admin user functionalities
 
+#endif
 
 
 
