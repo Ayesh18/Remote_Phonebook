16c016a2942bafea2f4ff6046642cc3c8c71bfeb
sainathreddy <70091863+sainath099@users.noreply.github.com>
2022-10-20 00:41:06 +0530

    Update user.h

--- Remote phonebook/CUT/Code/header/user.h
+++ Remote phonebook/CUT/Code/header/user.h
@@ -4,7 +4,8 @@
 **    DESCRIPTION    :      This File contains user header files.
 *****************************************************************************************/
 
-
+#ifndef USER_H
+#define USER_H
 int AuthenticatedAddData(char *,char *,char *);		//to add data to the phonebook
 int ToRemoveContact(char *,char *);					//to remove contact
 int Tolistcontact(char * ,char *,char *);			//to list the data from the group
@@ -13,6 +14,5 @@ int ToAddGrp(char *);								//to add a group
 int ToRemoveGrp(char *);							//to delete a group
 int ToAddData(char *,char *,char *);				//to add data to the database from anonymous,admin
 int AdminRemoveContact(char *,char *);
-
 int Adminlistcontact(char *, char *,char *);
-
+#endif
