ef8f0a26d24bd7324672c48de3dfbf971c7d1203
sainathreddy <70091863+sainath099@users.noreply.github.com>
2022-10-20 00:37:47 +0530

    Update client.h

--- Remote phonebook/CUT/Code/header/client.h
+++ Remote phonebook/CUT/Code/header/client.h
@@ -4,6 +4,8 @@
 **    DESCRIPTION    :      This File contains client header files.
 *****************************************************************************************/
 
+#ifndef CLIENT_H
+#define CLIENT_H
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -12,11 +14,9 @@ void Client();
 int ToGetSockfd();				//to get sock id
 void ToGetCredentials(char *);			//to get credentials
 int ToServerConnect( );				//to connect to the server
-int ToSendData(char *);			//to send data to the server
-//char * CToRecvData();				//to receive data from the server
-void CToRecvData(char *);
+int ToSendData(char *);			        //to send data to the server			
+void CToRecvData(char *);                       //to receive data from the server
 void ToGetUserCommands(char *, char *);		//to get user subcommands
 void ToDisplayRecvData(char * );		//to display records from the server
 int ToCloseClientConnections();			//to close the client connections
-
-        	
+#endif     	
