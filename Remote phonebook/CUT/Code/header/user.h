8dad513b89cf235fde3e845e9361a288ab7eb343
vinaykumarj21 <95273567+vinaykumarj21@users.noreply.github.com>
2022-10-19 22:27:27 +0530

    Update user.h

--- Remote phonebook/CUT/Code/header/user.h
+++ Remote phonebook/CUT/Code/header/user.h
@@ -1,5 +1,8 @@
-//program to have user header file
-
+/********************************************************************************************
+**     FILENAME      :      fuser.h
+**
+**    DESCRIPTION    :      This File contains user header files.
+*****************************************************************************************/
 int AuthenticatedAddData(char *,char *,char *);		//to add data to the phonebook
 int ToRemoveContact(char *,char *);					//to remove contact
 int Tolistcontact(char * ,char *,char *);			//to list the data from the group
@@ -7,5 +10,7 @@ void ToChgrp(char *,char *);						//to change the groups
 int ToAddGrp(char *);								//to add a group
 int ToRemoveGrp(char *);							//to delete a group
 int ToAddData(char *,char *,char *);				//to add data to the database from anonymous,admin
-int AdminRemoveContact(char *,char *);
-int Adminlistcontact(char *, char *,char *);
+int AdminRemoveContact(char *,char *);
+
+int Adminlistcontact(char *, char *,char *);
+
