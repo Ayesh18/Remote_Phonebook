ed0e01650c0cd610d8496de229df5c0b2cf56f26
vinaykumarj21 <95273567+vinaykumarj21@users.noreply.github.com>
2022-10-19 22:36:36 +0530

    Update server.h

--- Remote phonebook/CUT/Code/header/server.h
+++ Remote phonebook/CUT/Code/header/server.h
@@ -18,21 +18,21 @@
 #define ADDRSERV "127.0.0.1"
 #define MAX 256
 #define PORT 8028
-												//default destructor
-		void load();	    										//load the data from the file
+											
+void load();	    										//load the data from the file
 
-		int ToCreateSocket();											//create the socket
-		int ToBindSocket();											//to bind the socket	
-		int ToListen(); 											//to listen to the socket 
+int ToCreateSocket();											//create the socket
+int ToBindSocket();											//to bind the socket	
+int ToListen(); 											//to listen to the socket 
 		
-		void ToAcceptConnections();										//to accept connections
-        void SToRecvData(char *);											//to receive data and commands from client
-        void ToAuthenticateUser();										//to authenticate the user
+void ToAcceptConnections();										//to accept connections
+void SToRecvData(char *);										//to receive data and commands from client
+void ToAuthenticateUser();										//to authenticate the user
 		
 
-		void AuthenticatedUserFunctionalities(char *,char * ,char * );			//to perform authenticated user functionalities
-		void AnonymousFunctions(char *recvdata,char * filename);						//to perform anonymous user functionalities
-		void AdminFunction(char *recvdata,char * filename);							//to perform admin user functionalities
+void AuthenticatedUserFunctionalities(char *,char * ,char * );			//to perform authenticated user functionalities
+void AnonymousFunctions(char *recvdata,char * filename);			//to perform anonymous user functionalities
+void AdminFunction(char *recvdata,char * filename);				//to perform admin user functionalities
 		
 
 
