62cef42e2e865bb04ec3a2cdc143a7fa1d737714
sainathreddy <70091863+sainath099@users.noreply.github.com>
2022-10-19 23:46:51 +0530

    Update servermain.c

--- Remote phonebook/CUT/Code/src/servermain.c
+++ Remote phonebook/CUT/Code/src/servermain.c
@@ -1,25 +1,25 @@
-//program to define the server main function
 /**********************************************************************************
+
 	FILENAME: servermain.c
 	DESCRIPTION:This file is used to define the server main function.
 	DATE            NAME              REASON
 	------------------------------------------------------------------
     14/10/2022      Username          
  
- * ********************************************************************************/
+ ********************************************************************************/
 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "server.h"
+#include <server.h>
 
 int main()
 {	
-   	load();		        		//calling the function loadDAta of server
+   	load();		        		//calling the function load of server
 	ToCreateSocket();			//calling the function createSocket() of the server to create the socket
 	ToBindSocket();				//calling the function bindSocket to bind the socket
 	ToListen(); 				//calling the function Tolisten of the server 
-	ToAcceptConnections() ;		//calling the acceptConnection of the server 
+	ToAcceptConnections() ;			//calling the acceptConnection of the server 
 	return EXIT_SUCCESS;
 }
